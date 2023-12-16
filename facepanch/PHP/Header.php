3750ab2ba5a57a600b77f3f848ef85348d13cedd
Vlad2815 <144323119+Vlad2815@users.noreply.github.com>
2023-12-16 17:54:42 +0300

    Add files via upload

---
+++ facepanch/PHP/Header.php
@@ -0,0 +1,10 @@
+<header>
+			<img class='logo' src="img/logo.png" alt="">
+			<span class='ltext'>facepanch</span>
+				<div class='about'>
+					<a class='new' href="about.html" > новости </a>
+					<a class='game'> игры </a>
+					<a class='blog'> события и обновления </a>
+					<a class='help'> помощь </a>
+				</div>
+</header>
---
+++ facepanch/js/script.js
@@ -0,0 +1,2 @@
+function openMyWindow (){
+	var PopUpW = window.open("file:///C:/Users/79180/Desktop/влад/facepanch/cfg.txt", "PopUp Window", "resizable=no,scrollbars=no,width=350,height=150,copyhistory=1"); }	
